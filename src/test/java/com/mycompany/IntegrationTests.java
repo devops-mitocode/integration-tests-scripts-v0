dff8a2fb82ed091e53f071273ac0520e9bc53ccb
danycenas <cvdany7@gmail.com>
2025-09-23 21:49:29 -0500

    .

--- src/test/java/com/mycompany/IntegrationTests.java
+++ src/test/java/com/mycompany/IntegrationTests.java
@@ -1,14 +1,16 @@
 package com.mycompany;
 
+import io.cucumber.junit.CucumberOptions;
 import org.junit.platform.suite.api.*;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.*;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.mycompany.steps")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty,timeline:build/test-results/timeline")
+// @ConfigurationParameter(key = SNIPPET_TYPE_PROPERTY_NAME, value = "camelcase")
+@ConfigurationParameter(key = SNIPPET_TYPE_PROPERTY_NAME, value = "underscore")
 public class IntegrationTests {
 }
